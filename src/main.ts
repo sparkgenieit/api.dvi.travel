6a503b8e59884b4189e30ee96fe15aedea1fdd1e
kkversile <kiran.phpfish@gmail.com>
2025-12-10 15:42:03 +0530

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -74,7 +74,7 @@ async function bootstrap() {
   await prisma.enableShutdownHooks(app);
 
   const port = Number(process.env.PORT) || 4006;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`Server running on http://localhost:${port}`);
   console.log(`Swagger docs at http://localhost:${port}/docs`);
 }
